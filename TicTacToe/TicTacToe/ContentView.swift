e72249c23f8b96a141b0e583e4364988a5c6df89
Vladyslav Shkodych <vladyslav.shkodych@jmind.systems>
2022-06-05 15:51:09 +0300

    [add] blank new project

---
+++ TicTacToe/TicTacToe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Vlad Shkodich on 05.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Vlad Shkodich on 05.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
